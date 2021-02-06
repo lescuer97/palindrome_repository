8d7656645a2b36aa2a8253a38a92a8e6f562a00e
lescuer97 <leoescuer@protonmail.com>
2021-02-06 18:37:07 +0100

    gitignore, and index.ts
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

---
+++ index.ts
@@ -0,0 +1,20 @@
+
+//  @deno-types="./palindrom_wasm/out/palindrom_wasm.d.ts";
+
+// This is a web assembly function built in Rust
+import { searcher } from "./palindrom_wasm/deno_wasm/palindrom_wasm.js";
+
+//typescript palindrome searcher
+// import {searcher} from './searcher.ts';
+
+// input of the limit number
+
+const initialDate =  Date.now();
+console.log(searcher(99999999));
+// searcher(1112); 
+const finalDate = Date.now();
+console.log("Miliseconds: ",finalDate - initialDate);
+
+// SMALL TEST OF SPEED with input of 99999999: 
+    // WASM: 28978, 30456, 29845
+    // TS: 19377, 19624, 18488
